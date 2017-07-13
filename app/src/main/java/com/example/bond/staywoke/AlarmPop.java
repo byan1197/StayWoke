7692eac8a049c0c1608ac0f9753b264e25d5d105
BZJohnCen <john.cen@hotmail.com>
2017-07-13 04:12:04 -0400

    UI updated

--- app/src/main/java/com/example/bond/staywoke/AlarmPop.java
+++ app/src/main/java/com/example/bond/staywoke/AlarmPop.java
@@ -1,13 +1,17 @@
 package com.example.bond.staywoke;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.DisplayMetrics;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TimePicker;
 import android.widget.ToggleButton;
 
@@ -50,7 +54,7 @@ public class AlarmPop extends Activity {
         DatabaseHelper db = new DatabaseHelper(this);
         String repeatStr = "";
 
-        getWindow().setLayout((int)(width*.8), (int)(height*.8));
+        getWindow().setLayout((int)(width*.9), (int)(height*.9));
         Button saveButton = (Button) findViewById(R.id.saveBtn);
         Button exitButton = (Button) findViewById(R.id.exitBtn);
         tp = (TimePicker) findViewById(R.id.clockPicker);
@@ -157,6 +161,21 @@ public class AlarmPop extends Activity {
             }
         });
 
+    }
+
+    //Minimizes keyboard when touch on screen
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        View view = getCurrentFocus();
+        if (view != null && (ev.getAction() == MotionEvent.ACTION_UP || ev.getAction() == MotionEvent.ACTION_MOVE) && view instanceof EditText && !view.getClass().getName().startsWith("android.webkit.")) {
+            int scrcoords[] = new int[2];
+            view.getLocationOnScreen(scrcoords);
+            float x = ev.getRawX() + view.getLeft() - scrcoords[0];
+            float y = ev.getRawY() + view.getTop() - scrcoords[1];
+            if (x < view.getLeft() || x > view.getRight() || y < view.getTop() || y > view.getBottom())
+                ((InputMethodManager) this.getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow((this.getWindow().getDecorView().getApplicationWindowToken()), 0);
+        }
+        return super.dispatchTouchEvent(ev);
     }
 
 
